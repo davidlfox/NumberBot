3d698df3b440f6657bd5842d94af48d99b7fc9df
David L. Fox <me@iamdavidfox.com>
2013-08-24 14:37:32 -0400

    added usernames, some styling/cleanup

--- public/chat.js
+++ public/chat.js
@@ -4,22 +4,38 @@
     var field = document.getElementById('field');
     var sendButton = document.getElementById('send');
     var content = document.getElementById('content');
+    var name = document.getElementById('name');
 
     socket.on('message', function (data) {
         if (data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for (var i = 0; i < messages.length; i++) {
-                html += messages[i] + '<br />';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
+            content.scrollTop = content.scrollHeight;
         } else {
             console.log('there is some problem', data);
         }
     });
 
-    sendButton.onclick = function () {
-        var text = field.value;
-        socket.emit('send', { message: text });
+    sendButton.onclick = sendMessage = function () {
+        if (name.value == '') {
+            alert('please enter your name!');
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+            field.value = '';
+        }
     };
+
+    $(function () {
+        $('#field').keyup(function (e) {
+            if (e.keyCode == 13) {
+                sendMessage();
+            }
+        });
+    });
 }
