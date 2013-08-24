e2f5ac05979ab8a2ce51257fd7fbca154ec6f19f
David L. Fox <me@iamdavidfox.com>
2013-08-24 13:20:20 -0400

    number bot: installed express, jade, socket.io, created basic server

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var app = express();
+var port = 3700;
+
+app.get('/', function (request, response) {
+    response.send('it\'s alive!');
+});
+
+app.listen(port);
+console.log('listening on port: ' + port);
