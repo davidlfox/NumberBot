23c933e040992c29ccdd59d7b4b36c9b11608416
David L. Fox <me@iamdavidfox.com>
2013-08-25 20:55:40 -0400

    basic game functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 ﻿var express = require('express');
 var app = express();
 var port = 3700;
+var numberBot = require('./numberBot');
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', 'jade');
@@ -16,6 +17,18 @@ console.log('listening on port: ' + port);
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
+        // process game message
+        if (numberBot.isGameMessage(data)) {
+            data.message = data.username + ' is playing!';
+            data.username = numberBot.name;
+        }
+        if (numberBot.isGuessMessage(data)) {
+            console.log(numberBot.isUserPlaying(data.username));
+            if (numberBot.isUserPlaying(data.username)) {
+                data = numberBot.getGuessData(data);
+            }
+        }
+
         io.sockets.emit('message', data);
     });
 });
---
+++ numberBot.js
@@ -0,0 +1,53 @@
+﻿var name = 'Number Bot';
+
+var memory = {};
+
+exports.name = name;
+
+exports.isGameMessage = function (data) {
+    if (data.message) {
+        if (data.message == 'play') {
+            memory[data.username] = Math.floor(Math.random() * 9 + 1); // 1 to 10
+            return true;
+        }
+    }
+
+    return false;
+};
+
+exports.isGuessMessage = function (data) {
+    if (data.message) {
+        if (!isNaN(data.message)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+exports.isUserPlaying = function (name) {
+    return memory[name];
+};
+
+exports.getGuessData = function (data) {
+    var guess = parseInt(data.message);
+    console.log('memory[' + data.username + ']: ' + memory[data.username]);
+    console.log(guess < memory[data.username]);
+    console.log(guess > memory[data.username]);
+    console.log(guess == memory[data.username]);
+    if (guess < memory[data.username]) {
+        data.message = data.username + ', higher!';
+    } else if (guess > memory[data.username]) {
+        data.message = data.username + ', lower!';
+    } else if (guess == memory[data.username]) {
+        data.message = data.username + ', you got it!';
+        this.removePlayer(data.username);
+    }
+    data.username = this.name;
+
+    return data;
+};
+
+exports.removePlayer = function (name) {
+    delete memory[name];
+};
